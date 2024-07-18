a756fcf6a24a7bdd3fed874e274aab95d9ac06af
vincent <vincent.z.chao@gmail.com>
2024-07-18 15:29:36 +0800

    p80: solved

---
+++ src/main/java/com/binchaos/medium/p80/P80Solution.java
@@ -0,0 +1,56 @@
+package com.binchaos.medium.p80;
+
+/**
+ * [80. Remove Duplicates from Sorted Array
+ * II](https://leetcode.cn/problems/remove-duplicates-from-sorted-array-ii/description)
+ *
+ * 此解题思路过于复杂，且中间有多次元素移位的操作，不应采纳此方法
+ *
+ * @author Vincent
+ */
+@Deprecated
+public class P80Solution {
+	public int removeDuplicates(int[] nums) {
+		if (nums == null) {
+			throw new IllegalArgumentException("数组不能为 null");
+		}
+
+		if (nums.length == 0) {
+			return 0;
+		}
+
+		int count = 1;
+		int k = nums.length;
+
+		int i = 1;
+		while (i < k) {
+			if (nums[i] == nums[i - 1]) {
+				count++;
+			}
+
+			// 如果遍历到了下一个不同的元素，或者 整个数组已经遍历到了最后一个元素，则进行 count 的处理
+			if ((nums[i] != nums[i - 1]) || (i == k - 1)) {
+				if (count > 2) {
+					// 如果相同元素超过两个，则后续元素向前移动 delta 个
+					int delta = count - 2;
+
+					// 减掉多出的重复元素
+					k -= delta;
+
+					for (int j = i - delta; j < k; j++) {
+						nums[j] = nums[j + delta];
+					}
+
+					// 回到上一个重复元素的下一个元素
+					i = i - delta;
+				}
+
+				count = 1;
+			}
+
+			i++;
+		}
+
+		return k;
+	}
+}
---
+++ src/main/java/com/binchaos/medium/p80/P80Solution2.java
@@ -0,0 +1,42 @@
+package com.binchaos.medium.p80;
+
+/**
+ * [80. Remove Duplicates from Sorted Array
+ * II](https://leetcode.cn/problems/remove-duplicates-from-sorted-array-ii/description)
+ *
+ * @author Vincent
+ */
+public class P80Solution2 {
+	public int removeDuplicates(int[] nums) {
+		if (nums == null) {
+			throw new IllegalArgumentException("数组不能为 null");
+		}
+
+		int length = nums.length;
+		if (length <= 2) {
+			return length;
+		}
+
+		// 此方案的整体思路是：把当前数组当成一个空白数组，每一个位子都待赋值；
+		// 如果相同元素不超过两个，就赋原值；如果超过两个，就赋后面的值
+		// 方案：
+		// 使用 慢指针 表示当前需要被赋值的索引(最终也是此问题需要返回的 k 值，即最终剩下的元素个数)
+		// 使用 快指针 表示当前被遍历到的元素
+		// 使用快指针遍历整个数组，被遍历到的元素需要判断：是否与当前已处理得到的数组的 slow - 2 索引下的元素相等
+		// 相等则表示这个元素需要被去掉，不能保留；否则，需要被追加到当前已处理的数组的最后。
+
+		int slow = 2;
+		int fast = 2;
+
+		while (fast < length) {
+			if (nums[slow - 2] != nums[fast]) {
+				nums[slow] = nums[fast];
+				slow++;
+			}
+
+			fast++;
+		}
+
+		return slow;
+	}
+}
